5e13d83c73d14fec0e4a8a3d34c38b72a3058e86
GooseBoose <88116907+goose-TM@users.noreply.github.com>
2022-08-17 18:06:56 -0600

    Create JumbleCrypt.py

---
+++ JumbleCrypt.py
@@ -0,0 +1,96 @@
+import random
+while(True): 
+ userinput = input('string->')
+ charkey=random.randint(111111111,999999999)
+ AJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ BJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ CJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ DJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ EJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ FJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ GJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ HJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ IJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ JJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ KJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ LJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ MJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ NJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ OJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ PJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ QJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ RJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ SJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ TJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ UJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ VJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ WJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ XJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ YJUMBLEKEY = f'@{charkey}@'
+ charkey=random.randint(111111111,999999999)
+ ZJUMBLEKEY = f'@{charkey}@'
+ JUMBLEKEY=[AJUMBLEKEY ,BJUMBLEKEY ,CJUMBLEKEY ,DJUMBLEKEY ,EJUMBLEKEY ,FJUMBLEKEY ,GJUMBLEKEY ,HJUMBLEKEY ,IJUMBLEKEY ,JJUMBLEKEY ,KJUMBLEKEY ,LJUMBLEKEY ,MJUMBLEKEY ,NJUMBLEKEY ,OJUMBLEKEY ,PJUMBLEKEY ,QJUMBLEKEY ,RJUMBLEKEY ,SJUMBLEKEY ,TJUMBLEKEY ,UJUMBLEKEY ,VJUMBLEKEY ,WJUMBLEKEY ,XJUMBLEKEY ,YJUMBLEKEY ,ZJUMBLEKEY ]
+ jumblekeystr = str(JUMBLEKEY)
+ print(f'encrypting with {jumblekeystr}')
+ print('\n')
+ enctext = userinput.replace('A',AJUMBLEKEY).replace('B',BJUMBLEKEY).replace('C',CJUMBLEKEY).replace('D',DJUMBLEKEY).replace('E',EJUMBLEKEY).replace('F',FJUMBLEKEY).replace('G',GJUMBLEKEY).replace('H',HJUMBLEKEY).replace('I',IJUMBLEKEY).replace('J',JJUMBLEKEY).replace('K',KJUMBLEKEY).replace('L',LJUMBLEKEY).replace('M',MJUMBLEKEY).replace('N',NJUMBLEKEY).replace('O',OJUMBLEKEY).replace('P',PJUMBLEKEY).replace('Q',QJUMBLEKEY).replace('R',RJUMBLEKEY).replace('S',SJUMBLEKEY).replace('T',TJUMBLEKEY).replace('U',UJUMBLEKEY).replace('V',VJUMBLEKEY).replace('W',WJUMBLEKEY).replace('X',XJUMBLEKEY).replace('Y',YJUMBLEKEY).replace('Z',ZJUMBLEKEY).replace('A'.lower(),AJUMBLEKEY).replace('B'.lower(),BJUMBLEKEY).replace('C'.lower(),CJUMBLEKEY).replace('D'.lower(),DJUMBLEKEY).replace('E'.lower(),EJUMBLEKEY).replace('F'.lower(),FJUMBLEKEY).replace('G'.lower(),GJUMBLEKEY).replace('H'.lower(),HJUMBLEKEY).replace('I'.lower(),IJUMBLEKEY).replace('J'.lower(),JJUMBLEKEY).replace('K'.lower(),KJUMBLEKEY).replace('L'.lower(),LJUMBLEKEY).replace('M'.lower(),MJUMBLEKEY).replace('N'.lower(),NJUMBLEKEY).replace('O'.lower(),OJUMBLEKEY).replace('P'.lower(),PJUMBLEKEY).replace('Q'.lower(),QJUMBLEKEY).replace('R'.lower(),RJUMBLEKEY).replace('S'.lower(),SJUMBLEKEY).replace('T'.lower(),TJUMBLEKEY).replace('U'.lower(),UJUMBLEKEY).replace('V'.lower(),VJUMBLEKEY).replace('W'.lower(),WJUMBLEKEY).replace('X'.lower(),XJUMBLEKEY).replace('Y'.lower(),YJUMBLEKEY).replace('Z'.lower(),ZJUMBLEKEY)
+ print(f'Encrypted \n {enctext}')
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
